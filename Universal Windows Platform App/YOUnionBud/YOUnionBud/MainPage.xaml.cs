54fdace10d5ac9baa9b97f13b3a5028dc894ed8c
Dartegnian Velarde <dartegnian@users.noreply.github.com>
2017-12-02 19:23:52 +0800

    UWP stuff
    wew

---
+++ Universal Windows Platform App/YOUnionBud/YOUnionBud/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.Foundation.Metadata;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace YOUnionBud
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+
+    public sealed partial class MainPage : Page
+    {
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            if (ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+            {
+
+                var statusBar = StatusBar.GetForCurrentView();
+                if (statusBar != null)
+                {
+                    statusBar.BackgroundOpacity = 1;
+                    statusBar.BackgroundColor = Color.FromArgb(230, 152, 0, 1);
+                    statusBar.ForegroundColor = Colors.White;
+                }
+            }
+        }
+    }
+}
